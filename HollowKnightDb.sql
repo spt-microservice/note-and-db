3119e2b74c09444d6b335466a5a2a09f177f06eb
Samuel I. H. Sibuea <87352987+ifs20004-itdel@users.noreply.github.com>
2023-06-05 21:20:04 +0700

    Add files via upload

---
+++ HollowKnightDb.sql
@@ -0,0 +1,17 @@
+-- Insert into the hero_table --
+
+USE hollowknight;
+
+INSERT INTO hero_table VALUES(1,"Female","F4Jt5R","Charm Lover Salubra","https://static.wikia.nocookie.net/hollowknight/images/e/ea/Salubra.png/revision/latest/scale-to-width-down/350?cb=20180904182924");
+INSERT INTO hero_table VALUES(2,"Male","mBbQ24","Leg Eater","https://static.wikia.nocookie.net/hollowknight/images/f/f6/Legeater.png/revision/latest?cb=20210331084434");
+INSERT INTO hero_table VALUES(3,"Male","UCvmsE","Little Fool","https://static.wikia.nocookie.net/hollowknight/images/6/67/Little-Fool.png/revision/latest?cb=20170417115306");
+INSERT INTO hero_table VALUES(4,"Male","gaA5si","Sly","https://static.wikia.nocookie.net/hollowknight/images/f/f9/Sly.png/revision/latest?cb=20180924001337");
+INSERT INTO hero_table VALUES(5,"Male","Yskd7r","Relic Seeker Lemm","https://static.wikia.nocookie.net/hollowknight/images/e/ea/Lemm_Desk.png/revision/latest/scale-to-width-down/350?cb=20180916003218");
+INSERT INTO hero_table VALUES(6,"Male","f1ZrGa","The Last Stag","https://static.wikia.nocookie.net/hollowknight/images/d/de/Laststag.png/revision/latest/scale-to-width-down/350?cb=20200928205808");
+INSERT INTO hero_table VALUES(7,"Female","shhsbb","Steel Soul Jinn","https://static.wikia.nocookie.net/hollowknight/images/d/d6/SteelsoulJinn.png/revision/latest?cb=20170417145814");
+INSERT INTO hero_table VALUES(8,"Female","PWdLSx","Tuk","https://static.wikia.nocookie.net/hollowknight/images/a/a1/Tuk.png/revision/latest/scale-to-width-down/350?cb=20210331085512");
+INSERT INTO hero_table VALUES(9,"Male","aFJntr","Nailsmith","https://static.wikia.nocookie.net/hollowknight/images/f/f6/Nailsmith_2.png/revision/latest?cb=20180829024237");
+INSERT INTO hero_table VALUES(10,"Female","cgAUIM","Millibelle the Banker","https://static.wikia.nocookie.net/hollowknight/images/2/24/Millibelle.png/revision/latest?cb=20180528034357");
+
+
+INSERT INTO user_table VALUES(1, "Dummy","dummy@gmail.com", "dummy123","user");
